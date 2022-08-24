f96124e117d032a4a318b91bbee622aa293365f8
AntonKilk <ant.kilk@gmail.com>
2022-08-24 19:31:15 +0300

    cur ver find_paths.c

--- src/find_paths.c
+++ src/find_paths.c
@@ -6,38 +6,120 @@
 /*   By: akilk <akilk@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 12:24:25 by akilk             #+#    #+#             */
-/*   Updated: 2022/08/11 15:28:22 by akilk            ###   ########.fr       */
+/*   Updated: 2022/08/24 19:29:04 by akilk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lem_in.h"
 
-// char	*find_neighbours(int i, t_farm *farm)
+int	find_start(t_farm *farm)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < farm->rooms_nb)
+	{
+		if(!ft_strcmp(farm->start, farm->rooms[i]))
+			break;
+		i++;
+	}
+	return (i);
+}
+
+int	find_end(t_farm *farm)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < farm->rooms_nb)
+	{
+		if(!ft_strcmp(farm->end, farm->rooms[i]))
+			break;
+		i++;
+	}
+	return (i);
+}
+
+static void	lstadd(t_room **alst, t_room *new)
+{
+	if (alst == NULL)
+		return ;
+	new->next = *alst;
+	*alst = new;
+}
+
+static t_room	*lstnew(int room_nb)
+{
+	t_room	*ptr;
+
+	ptr = (t_room *)malloc(sizeof(t_room));
+	if (!ptr)
+		return (NULL);
+	ptr->room_nb = room_nb;
+	ptr->dist = -1;
+	ptr->next = NULL;
+	return (ptr);
+}
+
+// int	get_room_nb(t_farm *farm, int i)
 // {
-// 	if (farm->links[i] == '1')
-// 		return (farm->rooms[i]);
-// 	else
+// 	return (i % farm->rooms_nb);
 // }
+typedef struct queue
+{
+	int	head;
+	int	tail;
+	int	data[0];
+} t_queue;
+
+t_queue *new_queue(int size) {
+	t_queue *result;
+
+	result = malloc(sizeof(t_queue) + sizeof(int) * size);
+	if (!result)
+		return (NULL); //add error h
+	result->head = 0;
+	result->tail = 0;
+	return (result);
+}
+
+void	put(t_queue *q, int item)
+{
+	q->data[q->tail++] = item;
+}
+
+int	get(t_queue *q)
+{
+	return (q->data[q->head++]);
+}
+
+int	is_empty(t_queue *q)
+{
+	return (q->head >= q->tail);
+}
+
+void	release_queue(t_queue *q)
+{
+	free (q);
+}
 
 void	bfs(t_farm *farm)
 {
-	int		i;
-	int		k;
-	char	**visited;
+	t_queue *q;
+	t_room	*current;
+	int	i;
+	int	row;
 
+	q = new_queue(farm->rooms_nb);
 	i = 0;
-	visited = (char **)malloc(sizeof (char *) * (farm->rooms_nb + 1));
-	if (!visited)
-		return ;//add error
-	k = 0;
+	row = farm->rooms_nb * current->room_nb;
+	i += row;
 	while (i < farm->rooms_nb)
 	{
-		if (farm->links[i] == '1')
-		{
-			visited[k] = ft_strdup(farm->rooms[i]);
-			printf("%s\n", visited[k]);
-			k++;
-		}
+		put(q, i);
 		i++;
 	}
+	while (!is_empty(q))
+		printf("%d\n", get(q));
+	release_queue(q);
 }
